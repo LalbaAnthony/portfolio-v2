033139122b156ec6cdadf4df3147d5cf94576bd1
LalbaAnthony <anthony.lalba@limayrac.fr>
2023-11-13 21:26:49 +0100

    SUite

--- js/colors.js
+++ js/colors.js
@@ -8,6 +8,3 @@ function setPrimaryColor(color) {
     var r = document.querySelector(':root');
     r.style.setProperty('--primary', color);
 }
-
-//   const item = document.getElementById("itemId");
-//   item.addEventListener("click", function () { setPrimaryColor("#ba5a00") });
