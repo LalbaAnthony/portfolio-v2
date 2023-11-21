c51d500852d01e44dec0b64f868a89b33cef500c
LalbaAnthony <anthony.lalba@limayrac.fr>
2023-11-21 22:16:05 +0100

    Suite projets filter

--- js/github.js
+++ js/github.js
@@ -50,7 +50,7 @@ function formatRelativeDate(dateString) {
     return majSentence + timeSentence;
 }
 
-async function getProjects(orderby = 'name', order = 'asc') {
+async function getProjects(orderby = 'date', order = 'desc') {
     try {
         // Fetch projects
         const response = await fetch(`https://api.github.com/users/${username}/repos`);
@@ -100,57 +100,3 @@ async function getProjects(orderby = 'name', order = 'asc') {
         return [];
     }
 }
-
-async function fillProjectsList() {
-
-    const projectsList = document.getElementById('projects-list');
-    const projects = await getProjects();
-
-    projects.forEach(project => {
-        // Create list item
-        const listItem = document.createElement('li');
-        listItem.classList.add('project');
-
-        // Create project title
-        const title = document.createElement('h3');
-        title.classList.add('project-title');
-        title.textContent = project.name;
-
-        // Create language pill inside a span
-        const languageContainer = document.createElement('span');
-        const languagePill = document.createElement('div');
-        languagePill.classList.add('pill');
-        languagePill.style.backgroundColor = project.language_color;
-        languagePill.textContent = project.language;
-        languageContainer.appendChild(languagePill);
-
-        // Create project infos container
-        const infosContainer = document.createElement('div');
-        infosContainer.classList.add('project-infos');
-
-        // Create relative update time span
-        const updateSpan = document.createElement('span');
-        updateSpan.textContent = project.updated_at_relative;
-
-        // Append elements to the list item
-        infosContainer.appendChild(languageContainer);
-        infosContainer.appendChild(updateSpan);
-
-        listItem.appendChild(title);
-        listItem.appendChild(infosContainer);
-
-        // Append elements to the list item
-        listItem.addEventListener("click", function () {
-            window.open(project.url);
-        });
-
-        // Append the list item to the projects list
-        projectsList.appendChild(listItem);
-    });
-}
-
-window.addEventListener('DOMContentLoaded', function () {
-    // const projects = getProjects();
-    // console.log(projects);
-    fillProjectsList();
-});
--- js/pages/projets.js
+++ js/pages/projets.js
@@ -25,3 +25,57 @@ function closeFilters() {
     const filterDropdown = document.getElementById("filter-dropdown");
     filterDropdown.classList.remove("open");
 }
+
+async function fillProjectsList(orderby = 'date', order = 'desc') {
+
+    const projectsList = document.getElementById('projects-list');
+    const projects = await getProjects(orderby, order);
+
+    projects.forEach(project => {
+        // Create list item
+        const listItem = document.createElement('li');
+        listItem.classList.add('project');
+
+        // Create project title
+        const title = document.createElement('h3');
+        title.classList.add('project-title');
+        title.textContent = project.name;
+
+        // Create language pill inside a span
+        const languageContainer = document.createElement('span');
+        const languagePill = document.createElement('div');
+        languagePill.classList.add('pill');
+        languagePill.style.backgroundColor = project.language_color;
+        languagePill.textContent = project.language;
+        languageContainer.appendChild(languagePill);
+
+        // Create project infos container
+        const infosContainer = document.createElement('div');
+        infosContainer.classList.add('project-infos');
+
+        // Create relative update time span
+        const updateSpan = document.createElement('span');
+        updateSpan.textContent = project.updated_at_relative;
+
+        // Append elements to the list item
+        infosContainer.appendChild(languageContainer);
+        infosContainer.appendChild(updateSpan);
+
+        listItem.appendChild(title);
+        listItem.appendChild(infosContainer);
+
+        // Append elements to the list item
+        listItem.addEventListener("click", function () {
+            window.open(project.url);
+        });
+
+        // Append the list item to the projects list
+        projectsList.appendChild(listItem);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    // const projects = getProjects();
+    // console.log(projects);
+    fillProjectsList('nom', 'desc');
+});
